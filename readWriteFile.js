431a52712415df38efd74102735f6f482c6d1f75
Arpit Tomar <arpittomar7740@gmail.com>
2020-09-17 23:37:31 +0530

    Video - 9  :- write read file async

--- readWriteFile.js
+++ readWriteFile.js
@@ -2,4 +2,16 @@ const fs = require("fs");
 
 const readFileSync = fs.readFileSync("README.md", "utf-8");
 console.log(readFileSync);
-const writeFileSync = fs.writeFileSync("asyncWriteFile.txt", readFileSync);
+const writeFileSync = fs.writeFileSync("syncWriteFile.txt", readFileSync);
+
+fs.readFile("README.md", "utf-8", (err, data) => {
+  if (data) {
+    console.log(data);
+    fs.writeFile("asyncWriteFile.txt", data, (error, res) => {
+      if (error) throw error;
+      console.log("file has been saved");
+    });
+  } else {
+    console.log(err);
+  }
+});
