7547e17161c594a74c2a44a680e08fc2efa766d8
Arpit Tomar <arpittomar7740@gmail.com>
2020-09-20 00:46:53 +0530

    Readable streams

--- app.js
+++ app.js
@@ -1,10 +1,21 @@
 const http = require("http");
+const fs = require("fs");
 
-let server = http.createServer((req, res) => {
-  console.log(req.url);
-  res.writeHead(200, { "Content-Type": "text/plain" });
-  res.end("Hey guys");
+const myReadStream = fs.createReadStream(
+  __dirname + "/readBuffer.txt",
+  "utf-8"
+);
+
+myReadStream.on("data", (chunk) => {
+  console.log("new chunk received");
+  console.log(chunk);
 });
 
-server.listen(3000, "127.0.0.1");
-console.log("Listening to port 3000");
+// let server = http.createServer((req, res) => {
+//   console.log(req.url);
+//   res.writeHead(200, { "Content-Type": "text/plain" });
+//   res.end("Hey guys");
+// });
+
+// server.listen(3000, "127.0.0.1");
+// console.log("Listening to port 3000");
