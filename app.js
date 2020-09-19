e241f6b9be1c05122b6ae3b1596a7f898b273487
Arpit Tomar <arpittomar7740@gmail.com>
2020-09-20 02:28:12 +0530

    Send json

--- app.js
+++ app.js
@@ -15,11 +15,20 @@ const fs = require("fs");
 
 //use pipe on readable streams
 // myReadStream.pipe(myWriteStream);
+const obj = {
+  firstName: "Arpit",
+  lastName: "Tomar",
+};
 
 const server = http.createServer((req, res) => {
-  res.writeHead(200, { "Content-Type": "text/html" });
-  const myReadStream = fs.createReadStream(__dirname + "/index.html", "utf-8");
-  myReadStream.pipe(res);
+  res.writeHead(200, { "Content-Type": "application/json" });
+  const obj = {
+    firstName: "Arpit",
+    lastName: "Tomar",
+  };
+  //   const myReadStream = fs.createReadStream(__dirname + "/index.html", "utf-8");
+  //   myReadStream.pipe(res);
+  res.end(JSON.stringify(obj));
 });
 
 server.listen(3000, "127.0.0.1");
