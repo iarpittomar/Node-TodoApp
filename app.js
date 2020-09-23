f5864a07508a27774c618db1b493792241b1f6c2
Arpit Tomar <arpittomar7740@gmail.com>
2020-09-23 12:51:26 +0530

    get with express

--- app.js
+++ app.js
@@ -1,35 +1,45 @@
-const http = require("http");
-const fs = require("fs");
+// const http = require("http");
+// const fs = require("fs");
 
-// const myReadStream = fs.createReadStream(
-//   __dirname + "/readBuffer.txt",
-//   "utf-8"
-// );
+// // const myReadStream = fs.createReadStream(
+// //   __dirname + "/readBuffer.txt",
+// //   "utf-8"
+// // );
 
-// const myWriteStream = fs.createWriteStream(__dirname + "/writeBuffer.txt");
+// // const myWriteStream = fs.createWriteStream(__dirname + "/writeBuffer.txt");
 
-// myReadStream.on("data", (chunk) => {
-//   console.log("new chunk received");
-//   myWriteStream.write(chunk);
+// // myReadStream.on("data", (chunk) => {
+// //   console.log("new chunk received");
+// //   myWriteStream.write(chunk);
+// // });
+
+// //use pipe on readable streams
+// // myReadStream.pipe(myWriteStream);
+// const obj = {
+//   firstName: "Arpit",
+//   lastName: "Tomar",
+// };
+
+// const server = http.createServer((req, res) => {
+//   res.writeHead(200, { "Content-Type": "application/json" });
+//   const obj = {
+//     firstName: "Arpit",
+//     lastName: "Tomar",
+//   };
+//   //   const myReadStream = fs.createReadStream(__dirname + "/index.html", "utf-8");
+//   //   myReadStream.pipe(res);
+//   res.end(JSON.stringify(obj));
 // });
 
-//use pipe on readable streams
-// myReadStream.pipe(myWriteStream);
-const obj = {
-  firstName: "Arpit",
-  lastName: "Tomar",
-};
+// server.listen(3000, "127.0.0.1");
+// console.log("Listening to port 3000");
 
-const server = http.createServer((req, res) => {
-  res.writeHead(200, { "Content-Type": "application/json" });
-  const obj = {
-    firstName: "Arpit",
-    lastName: "Tomar",
-  };
-  //   const myReadStream = fs.createReadStream(__dirname + "/index.html", "utf-8");
-  //   myReadStream.pipe(res);
-  res.end(JSON.stringify(obj));
+var express = require("express");
+
+var app = express();
+
+app.get("/", (req, res) => {
+  res.send("This is the hompage");
 });
 
-server.listen(3000, "127.0.0.1");
-console.log("Listening to port 3000");
+app.listen(3000);
